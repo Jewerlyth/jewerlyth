10418b2c24ef9d74e77d6464475daa24d1d497f0
Jewerlyth <jewerlyth@gmail.com>
2025-05-03 16:31:13 -0500

    Actualizacion 7

--- Jewerlythweb/settings.py
+++ Jewerlythweb/settings.py
@@ -95,12 +95,14 @@ WSGI_APPLICATION = 'Jewerlythweb.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://jewerlyth_user:y9VwD1QDToQkPdUug90paMSMs2xMDwc8@dpg-d0ar6m6uk2gs73c3j3ag-a.virginia-postgres.render.com/jewerlyth'),
-    #'default':  dj_database_url.config(default=os.environ.get('postgresql://jewerlyth_user:y9VwD1QDToQkPdUug90paMSMs2xMDwc8@dpg-d0ar6m6uk2gs73c3j3ag-a.virginia-postgres.render.com/jewerlyth')),
-    #para modificar la base de datos y visualizarlan aqui en django
-    'OPTIONS': {
-            'client_encoding': 'UTF8',  # Asegúrate de que este parámetro esté presente
-    },
+    'default': dj_database_url.config(
+        default='postgresql://jewerlyth_user:y9VwD1QDToQkPdUug90paMSMs2xMDwc8@dpg-d0ar6m6uk2gs73c3j3ag-a.virginia-postgres.render.com/jewerlyth'
+    )
+}
+
+# Opcional: si quieres añadir opciones específicas
+DATABASES['default']['OPTIONS'] = {
+    'client_encoding': 'UTF8',
 }
 
 
